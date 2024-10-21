aaae8674323418026641ad2262ec49d67e91ee8c
SwayamMishra23 <swayamm9shra@gmail.com>
2024-10-21 23:03:23 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,53 @@
+function locomotiveAnimation() {
+    gsap.registerPlugin(ScrollTrigger);
+
+    const locoScroll = new LocomotiveScroll({
+        el: document.querySelector("#main"),
+        smooth: true,
+
+        // for tablet smooth
+        tablet: { smooth: true },
+
+        // for mobile
+        smartphone: { smooth: true }
+    });
+    locoScroll.on("scroll", ScrollTrigger.update);
+
+    ScrollTrigger.scrollerProxy("#main", {
+        scrollTop(value) {
+            return arguments.length
+                ? locoScroll.scrollTo(value, 0, 0)
+                : locoScroll.scroll.instance.scroll.y;
+        },
+        getBoundingClientRect() {
+            return {
+                top: 0,
+                left: 0,
+                width: window.innerWidth,
+                height: window.innerHeight
+            };
+        }
+    });
+
+    ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+
+    ScrollTrigger.refresh();
+}
+locomotiveAnimation();
+
+document.addEventListener("mousemove", function (e) {
+    const cursorText = document.getElementById("cursor-text");
+
+    // Set the position of the text slightly offset from the cursor
+    const offset = 5; // You can adjust this offset to your preference
+    cursorText.style.left = (e.clientX + offset) + "px";
+    cursorText.style.top = (e.clientY + offset) + "px";
+});
+
+document.querySelector("#moving-div").addEventListener("mouseenter", function () {
+    document.getElementById("cursor-text").style.display = "block";
+});
+
+document.querySelector("#moving-div").addEventListener("mouseleave", function () {
+    document.getElementById("cursor-text").style.display = "none";
+});
